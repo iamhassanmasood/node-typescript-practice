20306e864f0ee596c954894ddf562ec3a21026e0
hassan-anzer-ai <hassan@anzer.ai>
2023-10-10 20:35:40 +0500

    auth middleware created

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,46 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
-import { z } from 'zod';
+import jwt from 'jsonwebtoken';
 
-const signupSchema = z.object({
-  firstName: z
-    .string()
-    .min(4, { message: 'First name must contain 4 characters ' })
-    .max(255, { message: 'Exceed characters limit' })
-    .optional(),
-  lastName: z
-    .string()
-    .min(4, { message: 'last name must contain 4 characters ' })
-    .max(255, { message: 'Exceed characters limit' })
-    .optional(),
-  username: z
-    .string()
-    .min(4, { message: 'Username must contain 4 characters ' })
-    .max(255, { message: 'Exceed characters limit' })
-    .optional(),
-  email: z.string().email({ message: 'Invalid email address' }),
-  password: z
-    .string()
-    .min(8, { message: 'Password must contain 8 characters' })
-    .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).*$/, {
-      message: `Password must contain 8 characters, one uppercase letter, one lowercase letter, one numeric digit, one symbol`,
-    })
-    .optional(),
-});
-
-export const validateSignup = (req: Request, res: Response, next: NextFunction) => {
+export const checkAuthentication = (request: Request, response: Response, next: NextFunction) => {
   try {
-    signupSchema.parse(req.body);
+    const token = request.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
     return next();
   } catch (error) {
-    return res.status(400).json({ error });
+    return response.status(401).json({
+      message: 'Authentication failed',
+    });
   }
-};
-
-let isAutheticated: boolean = true;
-
-export const checkAuthentication = (_req: Request, res: Response, next: NextFunction) => {
-  if (isAutheticated) {
-    return next();
-  }
-  return res.status(401).json({ message: 'Unauthorized' });
 };
