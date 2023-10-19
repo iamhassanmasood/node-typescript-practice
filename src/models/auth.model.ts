29649b145f468c211d6a246cde6694c312912d61
Hassan Masood <iamhassanmasood@gmail.com>
2023-10-19 15:30:46 +0500

    schema updated

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -9,27 +9,11 @@ export interface IUser extends Document {
 }
 
 const authUserSchema = new Schema<IUser>({
-  firstName: { type: String, required: [true, 'First name is required'] },
-  lastName: { type: String, required: [true, 'Last name is required'] },
-  username: { type: String, required: [true, 'Username is required'], unique: true },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    validate: {
-      validator: (value: string) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value),
-      message: 'Invalid email format',
-    },
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    validate: {
-      validator: (value: string) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/.test(value),
-      message:
-        'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one numeric digit, and one special symbol.',
-    },
-  },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 export default mongoose.model<IUser>('AuthUser', authUserSchema);
