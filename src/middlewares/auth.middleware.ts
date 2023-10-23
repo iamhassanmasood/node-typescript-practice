c3fdeaba39cb37ed3640959d8a11e75a6836f63e
Hassan Masood <iamhassanmasood@gmail.com>
2023-10-23 18:14:20 +0500

    warning fixed

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const checkAuthentication = (request: Request, response: Response, next: NextFunction) => {
+export const checkAuthentication = (request: Request, response: Response, next: NextFunction): void => {
   try {
     const token = request.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded);
-    return next();
+    next();
   } catch (error) {
-    return response.status(401).json({
+    response.status(401).json({
       message: 'Authentication failed',
     });
   }
