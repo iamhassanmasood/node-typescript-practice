fc3f0815d39bda27320bb80a5f08ffa0c67ba345
Hassan Masood <iamhassanmasood@gmail.com>
2023-10-30 19:51:00 +0500

    some values added

--- src/models/places.model.ts
+++ src/models/places.model.ts
@@ -1,8 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
 
 const PlacesSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  title: { type: String, required: true },
+  location: { type: String },
   url: { type: String, required: true },
+  article: { type: String, required: true },
+  images: { type: Array },
 });
 
 export default mongoose.model('Places', PlacesSchema);
