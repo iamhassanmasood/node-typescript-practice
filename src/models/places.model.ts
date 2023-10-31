1c6ce5a3280171cbac866a00747895715dc22ead
Hassan Masood <iamhassanmasood@gmail.com>
2023-10-31 21:40:56 +0500

    interface added

--- src/models/places.model.ts
+++ src/models/places.model.ts
@@ -1,11 +1,19 @@
 import mongoose, { Schema } from 'mongoose';
 
+export interface IPlaces extends Document {
+  title: string;
+  place: string;
+  url: string;
+  article: string;
+  // images: array;
+}
+
 const PlacesSchema: Schema = new Schema({
   title: { type: String, required: true },
-  location: { type: String },
+  place: { type: String },
   url: { type: String, required: true },
   article: { type: String, required: true },
-  images: { type: Array },
+  // images: { type: Array },
 });
 
-export default mongoose.model('Places', PlacesSchema);
+export default mongoose.model<IPlaces>('Places', PlacesSchema);
