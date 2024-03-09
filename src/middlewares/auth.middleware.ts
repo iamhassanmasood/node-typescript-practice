47eac4aa708686c9688af36af8724da95e876de6
Hassan Masood <iamhassanmasood@gmail.com>
2024-03-09 23:43:37 +0500

    console added

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,7 +5,7 @@ export const checkAuthentication = (request: Request, response: Response, next: 
   try {
     const token = request.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
+    console.log({ decoded }, 'Test Decode');
     return next();
   } catch (error) {
     return response.status(401).json({
